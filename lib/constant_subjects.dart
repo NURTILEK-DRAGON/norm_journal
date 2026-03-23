849b0a3d7532436e2fc5395063b71bb093d4c38a
NURTILEK-DRAGON <dragonovdragon09@gmail.com>
2026-03-23 11:13:03 +0600

    teacher regi-on page

---
+++ lib/constant_subjects.dart
@@ -0,0 +1,13 @@
+class ConstantSubjects {
+  static const List<String> availableSubjects = [
+    'math',
+    'physics',
+    'chemistry',
+    'biology',
+    'geography',
+    'history',
+    'english',
+    'russian',
+    'biology',
+  ];
+}
