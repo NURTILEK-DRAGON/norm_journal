49cbf610cf06d8c7f763a1324eafc8caacfa02f1
NURTILEK-DRAGON <dragonovdragon09@gmail.com>
2026-03-30 10:19:18 +0600

    two methods for student list

--- lib/data/repository/firestore_service.dart
+++ lib/data/repository/firestore_service.dart
@@ -53,4 +53,35 @@ Stream<List<Map<String, dynamic>>> getMonitorsStream() {
       .snapshots() // Подписываемся на обновления в реальном времени
       .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
 }
+
+// 1. Сохраняем список студентов для конкретной группы
+Future<void> saveStudentList(String groupId, List<String> students) async {
+  try {
+    // Мы создаем (или обновляем) документ, где ID документа — это название группы
+    await _db.collection('groups').doc(groupId).set({
+      'group_id': groupId,
+      'students': students,
+      'last_updated': FieldValue.serverTimestamp(),
+    });
+    _logger.i('Список студентов группы $groupId обновлен в Firestore');
+  } catch (e) {
+    _logger.e('Ошибка сохранения студентов: $e');
+    rethrow;
+  }
+}
+
+// 2. Получаем список студентов группы
+Future<List<String>> getStudentList(String groupId) async {
+  try {
+    DocumentSnapshot doc = await _db.collection('groups').doc(groupId).get();
+    if (doc.exists) {
+      List<dynamic> data = doc.get('students');
+      return data.cast<String>();
+    }
+    return [];
+  } catch (e) {
+    _logger.e('Ошибка получения студентов: $e');
+    return [];
+  }
+}
 }
